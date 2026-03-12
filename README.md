3fef732dadb554bd57a3bcf2ccbc5274ef00245b
George Gitura <geogitura@gmail.com>
2026-03-12 22:38:12 +0300

    Initial commit: Port Scanner in python

---
+++ README.md
@@ -0,0 +1,9 @@
+# Port Scanner
+
+A simpleport scanner in python that accepts a target, resolves its IPv4 address and then scans for open ports.
+
+## Usage
+python port_scanner.py <target>
+
+## Technologies
+Python
---
+++ port_scanner.py
@@ -0,0 +1,56 @@
+import pyfiglet
+import sys
+import socket
+from datetime import datetime
+
+# Style intro text with figlet
+ascii_banner = pyfiglet.figlet_format("Port Scanner")
+print(ascii_banner)
+
+# Check if the user provided the target IP address
+if len(sys.argv) == 2:
+
+    # Translate hostname to ipv4 address
+    target = socket.gethostbyname(sys.argv[1])
+else:
+    print("Invalid amount of arguments.")
+    print("Syntax: python3 port_scanner.py <ip>")
+    sys.exit()
+
+# Check start time
+t1 = datetime.now()
+
+# Add a  banner with info about the scan
+print("-" * 50)
+print("Scanning Target: " + target)
+print("Scanning started at: " + str(datetime.now()))
+print("-" * 50)
+
+try:
+
+    # Scan all ports between 1 to 65,535
+    for port in range(1,65535):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(1)
+
+        # Returns an error indicator
+        result = s.connect_ex((target,port))
+        if result == 0:
+            print("Port {} is open".format(port))
+        s.close()
+
+except KeyboardInterrupt:
+    print("\n Exitting Program !!!")
+    sys.exit()
+
+except socket.gaierror:
+    print("\n Hostname could not be resolved !!!")
+    sys.exit()
+
+except socket.error():
+    print("\n Server not responding !!!")
+    sys.exit()
+
+# Print duration
+print(f"Scanning completed in: {datetime.now() - t1}")
+
